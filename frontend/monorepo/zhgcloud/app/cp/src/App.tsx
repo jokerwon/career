3a33cf3b8446835d3f921e983dce3a0281ecda91
wengjiankai <wengjiankai@zeaho.com>
2022-05-18 18:36:58 +0800

    feat: 新增monorepo案例

---
+++ frontend/monorepo/zhgcloud/app/cp/src/App.tsx
@@ -0,0 +1,12 @@
+import { Foo } from '@zhgcloud/components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Foo title="Hello monorepo" />
+    </div>
+  );
+}
+
+export default App;
